dd9244cb5da92cafbf2f25fa08d99b6c30fee64d
Meshhur <meshhuratageldiyev010@gmail.com>
2022-06-24 20:42:09 +0500

    modified

---
+++ main.js
@@ -0,0 +1,16 @@
+// "use strict";
+// console.log(typeof(null));
+
+// let a = "HELLo";
+
+// function fff(a) {
+//     for (let i = 0; i <= a.length; i++) {
+//         let b = a.toLowerCase();
+//         if (a[i] == b) {
+//             return 'No';
+//         }
+//         else {
+//             return 'Yes'
+//         }
+//     }
+// }
