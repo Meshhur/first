fce968c8f52bff20019ace4e6bccd5bf8acce92d
Meshhur <meshhuratageldiyev010@gmail.com>
2022-06-24 21:30:41 +0500

    modified Js

--- main.js
+++ main.js
@@ -14,3 +14,42 @@
 //         }
 //     }
 // }
+
+"use strict";
+
+let video = document.querySelector('#video')
+let playPauseBtn = document.querySelector('#ppbtn')
+let muteBtn = document.querySelector('#mutebtn')
+let plusBtn = document.querySelector('#plusbtn')
+let minusBtn = document.querySelector('#minusbtn')
+let progress = document.querySelector('#progress')
+let progressBar = document.querySelector("progressBar");
+
+
+const startStopVideo = function () {
+    let videoState = playPauseBtn.getAttribute('data-video-controller');
+    let plpause = document.getElementById('ppbtn');
+
+    if (videoState === 'pause') {
+        video.play();
+        playPauseBtn.setAttribute('data-video-controller', 'play')
+        plpause.style.backgroundColor = 'lightgreen';
+    }
+    else {
+        video.pause();
+        plpause.style.backgroundColor = '#8598b1';
+        playPauseBtn.setAttribute('data-video-controller', 'pause')
+    }
+}
+
+playPauseBtn.addEventListener('click', startStopVideo)
+
+
+video.addEventListener('loadedmetadata', () => {
+    progress.setAttribute("max", video.duration)
+});
+
+video.addEventListener('timeupdate', () => {
+    progress.value = video.currentTime;
+    progressBar.style.width = Math.floor((video.currentTime / video.duration) * 100 + "%");
+});
